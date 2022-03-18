299e8423ee608c1ece4efa12dcd445b9d1122325
mixayloff-dimaaylov <65488726+mixayloff-dimaaylov@users.noreply.github.com>
2022-03-18 18:55:32 +0300

    Add temporary column `psrNt`

--- src/main/scala/com/infocom/examples/spark/TecCalculation.scala
+++ src/main/scala/com/infocom/examples/spark/TecCalculation.scala
@@ -355,7 +355,7 @@ object TecCalculation extends Serializable {
     val newDNT = range
       .withColumn("f1", f($"system", $"f1", $"glofreq"))
       .withColumn("f2", f($"system", $"f2", $"glofreq"))
-      .select($"time", $"sat", $"adr1", $"adr2", $"f1", $"f2", $"psr1", $"psr2")
+      .select($"time", $"sat", $"adr1", $"adr2", $"f1", $"f2", $"psr1", $"psr2", $"sdcb")
       .groupBy($"sat")
       .agg(avg(k($"adr1", $"adr2", $"f1", $"f2", $"psr1", $"psr2", $"sdcb")).as("K"))
       .select("K")
@@ -389,9 +389,15 @@ object TecCalculation extends Serializable {
          |  anyIf(freq, freq = '$f2Name') AS f2,
          |  any(system) AS system,
          |  any(glofreq) AS glofreq,
-         |  '$sigcomb' AS sigcomb
+         |  '$sigcomb' AS sigcomb,
+         |  ifNull(any(sdcb), 0) AS sdcb
          |FROM
          |  rawdata.range
+         |LEFT OUTER JOIN
+         |  misc.sdcb
+         |  ON (range.sat = sdcb.sat)
+         |  AND (range.system = sdcb.system)
+         |  AND (sigcomb = sdcb.sigcomb)
          |WHERE
          |  sat='$sat' AND d BETWEEN toDate($from/1000) AND toDate($to/1000) AND time BETWEEN $from AND $to
          |  and freq in ('$f1Name', '$f2Name')
@@ -416,7 +422,8 @@ object TecCalculation extends Serializable {
 
     val tecRange = range
       .withColumn("nt", NtFunctions.rawNt($"adr1", $"adr2", $"f1", $"f2", $"DNT"))
-      .select("time", "sat", "sigcomb", "f1", "f2", "nt")
+      .withColumn("psrNt", NtFunctions.psrNt($"psr1", $"psr2", $"f1", $"f2", $"sdcb"))
+      .select("time", "sat", "sigcomb", "f1", "f2", "nt", "psrNt")
 
     //tecRange.show()
 
